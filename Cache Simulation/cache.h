0818c70d3bcec79c30d4fb67a43a11dcc2d0f6a3
Jccarr3 <143148862+Jccarr3@users.noreply.github.com>
2025-09-23 22:14:15 -0400

    Add files via upload

--- Cache Simulation/cache.h
+++ Cache Simulation/cache.h
@@ -10,6 +10,7 @@ using namespace std;
 class cache{
     int size, assoc, blocksize, sets, BO, index, tag;
     int reads, read_misses, writes, write_misses, write_backs; //counters for statistics
+    cache *next_level; //pointer to next level of cache
 
 
     
@@ -25,7 +26,8 @@ class cache{
 
         class stream{
             public:
-                uint64_t tag;
+                vector<uint64_t> tags;      //vector of tags in stream buffer
+                uint64_t recency;           //recency of stream buffer
                 bool valid = false;
         };
 
@@ -33,11 +35,11 @@ class cache{
         vector<vector<block>> set_list;
 
         //vector of vector for stream buffers
-        vector<vector<stream>> stream_buffer;
+        vector<stream> stream_buffers;
 
 
         //constructor for cache class, should have following configurable parameters(size, associability, blocksize))
-        cache(int bytes = 0, int associability = 0 , int block = 0, int stream_count = 0, int stream_size = 0){
+        cache(int bytes = 0, int associability = 0 , int block = 0, int stream_count = 0, int stream_size = 0, cache *next = nullptr){
             size = bytes;                           //total size of cache
             assoc = associability;                  //number of blocks per set
             blocksize = block;                      //block size in bytes
@@ -50,134 +52,168 @@ class cache{
             }
 
             //initialize stream buffer
-            stream_buffer.resize(stream_count);
+            stream_buffers.resize(stream_count);            //resize stream buffer to number of stream buffers
             for(int i = 0; i < stream_count; i++){
-                stream_buffer[i].resize(stream_size); //resize each stream buffer to stream size
+                stream_buffers[i].tags.resize(stream_size); //resize each stream buffer to stream size
             }
         }
         //end of constructor code
 
-        //function for handling cache writes
-        void write(uint64_t address){
-            int lru_index = 0;                                      //variable to track LRU block
-            uint64_t lru_recency = set_list[index][0].recency;      //variable to track LRU recency
 
-            writes++;                                             //increment write counter
-            uint64_t index_mask = sets - 1;                       //mask to get index bits
-            index = (address / blocksize) & index_mask;           //calculate index
-            tag = address / (blocksize * sets);                   //calculate tag
-            bool cache_hit = false;                               //flag to check if hit or miss in cache
-            bool stream_hit = false;                              //flag to check if hit or miss in stream buffer
+        void request(char rw, uint64_t addy){   //function that handles read/write requests
+            //find tag and index bits
+            int BO_bits = log2(blocksize);                   //number of block offset bits
+            int index_bits = log2(sets);                     //number of index bits
+            int mask = sets - 1;                             //mask to get index bits
+            index = (addy / sets) & mask;                    //index bits
+            tag = (addy >> BO_bits) >> index_bits;           //tag bits
+            //find tag and index bits
 
-            //check for hit in the cache
-            for(int i = 0; i < assoc; i++){
-                if(set_list[index][i].tag == tag){
-                    cache_hit = true;                                 //set hit flag to true
-                    lru_index = i;                             //store index of the hit block
-                    //adjust recency of all blocks in the set
-                    for(int j = 0; j < assoc; j++){
-                        if((set_list[index][j].recency  < set_list[index][lru_index].recency) && set_list[index][j].valid){ //if block is more recent than the hit block
-                            set_list[index][j].recency++;       //increment recency of all valid blocks
-                            
+            bool cache_hit = false;                     //flag to check if cache hit or miss
+            bool stream_hit = false;                    //flag to check if stream buffer hit or miss
+            int LRU_index = 0;                         //index of least recently used block in set
+
+            //check for cache hit/
+            for(int i = 0; i < assoc; i++){             //loop through all blocks in the set
+                if(set_list[index][i].tag == tag){              //if block tags match
+                    cache_hit = true;                                           //cache hit
+                    for(int j = 0; j < stream_buffers.size(); j++){             //find stream to store new tags
+                        if(stream_buffers[j].recency == stream_buffers.size()){                          //if stream buffer is most recently used
+                            stream_store(stream_buffers, tag, j);                       //replace most recently used stream buffer
+                            break;                                            
+                        }
+                        if(!stream_buffers[j].valid){                               //if stream buffer is invalid
+                            stream_store(stream_buffers, tag, j);                    //store new tags in stream buffer
+                            break;                                                  //exit loop
+                        }
+                    }                                 
+
+                    if(rw == 'w'){                       //if write request
+                        set_list[index][i].dirty = true; //set dirty bit
+                        writes++;                        //increment write counter
+                    }
+                    else{
+                        reads++;                         //increment read counter
+                    }
+                    block_adjust(set_list[index], i);    //adjust recency of blocks in set
+                    return;                              //exit function
+                }
+            }
+            //check for cache hit
+
+            //if cache miss, check for stream buffer hit
+            if(!cache_hit){
+                for(int i = 0; i < stream_buffers.size(); i++){     //loop through all stream buffers
+                    if(stream_buffers[i].valid){                    //if stream buffer is valid
+                        for(int j = 0; j < stream_buffers[i].tags.size(); j++){ //loop through all tags in stream buffer
+                            if(stream_buffers[i].tags[j] == tag){   //if tags match
+                                stream_hit = true;                  //stream buffer hit
+                                //code here to move stream buffer block to cache
+                                stream_store(stream_buffers, tag, i);      //store new tags in stream buffer
+                                cache_store(set_list[index], tag);          //store new block in cache
+                                if(rw == 'w'){                       //if write request
+                                    writes++;                        //increment write counter
+                                    set_list[index][LRU_index].dirty = true; //set dirty bit                                   
+                                } else {
+                                    reads++;                         //increment read counter
+                                }
+                                return;                              //exit function
+                            }
                         }
                     }
-                    set_list[index][i].dirty = 1;               //set dirty bit to 1
-                    set_list[index][i].recency = 0;             //update recency
-                    break;
                 }
             }
-            //check for hit in the stream buffer
-            if(!cache_hit){                                        //only check stream buffer if miss in cache
-                for(int i = 0; i < stream_buffer.size(); i++){      //for each stream buffer
-                    for(int j = 0; j < stream_buffer[i].size(); j++){ //for each block in the stream buffer
-                        if(stream_buffer[i][j].tag == tag && stream_buffer[i][j].valid){ //if hit in stream buffer
-                            stream_hit = true;                          //set hit flag to true
-                            //move the block to the cache
-                            //empty block check***************************************************
-                            for(int k = 0; k < assoc; k++){
-                                if(!set_list[index][k].valid){                 //if empty block found
-                                    set_list[index][k].valid = 1;              //set valid bit to 1
-                                    set_list[index][k].tag = tag;              //set tag
-                                    set_list[index][k].dirty = 1;              //set dirty bit to 1
-                                    set_list[index][k].recency = 0;            //update recency
-                                    lru_index = k;                             //store index of the empty block
-                                    
-                                    //invalidate the block in the stream buffer
-                                    stream_buffer[i][j].valid = 0;
-                                    return;
-                                }
-                            }
-                            //empty block check***************************************************
+            //if stream buffer miss, handle cache miss
 
-                            //if no empty block, need to evict a block using LRU policy           
-                            
-                            //code finds lRU block
-                            for(int k = 0; k < assoc; k++){
-                                if(set_list[index][k].recency > lru_recency){
-                                    lru_recency = set_list[index][k].recency;
-                                    lru_index = k;
-                                }
-                            }
-
-                            //if the block to be evicted is dirty, increment write back counter
-                            if(set_list[index][lru_index].dirty){
-                                write_backs++;
-                            }
-
-                            //adjust recency of all blocks in the set
-                            for(int k = 0; k < assoc; k++){
-                                if(set_list[index][k].valid && k != lru_index){ //if block is valid and not the one being evicted
-                                    set_list[index][k].recency++;
-                                }
-                            }
-
-                            //move the block from stream buffer to cache
-                            set_list[index][lru_index].valid = 1;              //set valid bit
-
-            //handles write misses and updates recency
-            if(!cache_hit && !stream_hit){                                               //if miss
-                write_misses++;//be sure this does not occur if stream buffer hits                                    //increment write miss counter
-
-                //check for empty block in the set
-                for(int i = 0; i < assoc; i++){
-                    if(!set_list[index][i].valid){                 //if empty block found
-                        set_list[index][i].valid = 1;              //set valid bit to 1
-                        set_list[index][i].tag = tag;              //set tag
-                        set_list[index][i].dirty = 1;              //set dirty bit to 1
-                        set_list[index][i].recency = 0;            //update recency
-                        lru_index = i;                             //store index of the empty block
-                        return;
-                    }
+            //handle miss 
+            if(!cache_hit && !stream_hit){
+                if(next_level != nullptr){   //if there is a next level of cache
+                    next_level->request('r', addy); //send read request to next level of cache
                 }
-
-                //if no empty block, need to evict a block using LRU polic           
-                
-                //code finds lRU block
-                for(int i = 0; i < assoc; i++){
-                    if(set_list[index][i].recency > lru_recency){
-                        lru_recency = set_list[index][i].recency;
-                        lru_index = i;
+                cache_store(set_list[index], tag);          //store new block in cache
+                for(int j = 0; j < stream_buffers.size(); j++){             //find stream to store new tags
+                    if(stream_buffers[j].recency == stream_buffers.size()){                          //if stream buffer is most recently used
+                        stream_store(stream_buffers, tag, j);                       //replace most recently used stream buffer
+                        break;                                            
                     }
-                }
-
-                //if the block to be evicted is dirty, increment write back counter
-                if(set_list[index][lru_index].dirty){
-                    write_backs++;
-                }
-
-                //adjust recency of all blocks in the set
-                for(int i = 0; i < assoc; i++){
-                    if(set_list[index][i].valid && i != lru_index){ //if block is valid and not the one being evicted
-                        set_list[index][i].recency++;
+                    if(!stream_buffers[j].valid){                               //if stream buffer is invalid
+                        stream_store(stream_buffers, tag, j);                    //store new tags in stream buffer
+                        break;                                                  //exit loop
                     }
+                }                                 
+                if(rw == 'w'){                       //if write request
+                    writes++;                        //increment write counter
+                    write_misses++;                  //increment write miss counter
+                    set_list[index][LRU_index].dirty = true; //set dirty bit                                   
+                } else {
+                    reads++;                         //increment read counter
+                    read_misses++;                   //increment read miss counter
                 }
             }
-        }
-        //end of write function
 
-        //function for handling cache reads
+        }           
         
 
 };
 
 
+
+//function used to adjust recency of blocks in a vector
+void block_adjust(vector<cache::block> &v, int index){
+    for(int i = 0; i < v.size(); i++){
+        if(v[i].valid){
+            v[i].recency++;                 //increase the recency of all blocks older than the accessed block
+        }
+    }
+    v[index].recency = 0;               //set recency of accessed block to 0
+}
+//function used to adjust recency of blocks in a vector
+
+//function used to adjust recency of stream buffers in a vector
+void stream_adjust(vector<cache::stream> &v, int index){
+    for(int i = 0; i < v.size(); i++){
+        if(v[i].valid){
+            v[i].recency++;                 //increase the recency of all blocks older than the accessed block
+        }
+    }
+    v[index].recency = 0;               //set recency of accessed block to 0
+}
+//function used to adjust recency of stream buffers in a vector
+
+//function to store new items in stream 
+void stream_store(vector<cache::stream> &v, uint64_t tag, int stream){
+    for(int i = 0; i < v[stream].tags.size(); i++){
+        v[stream].tags[i] = tag+i+1;    //store new tags in stream buffer
+    }
+    v[stream].valid = true;            //set stream buffer to valid
+    stream_adjust(v, stream);          //adjust recency of stream buffers
+}
+//function to store new items in stream 
+
+//function to store new block in cache
+void cache_store(vector<cache::block> &v, uint64_t tag){          //set(not set_list) is passed as argument
+    for(int i = 0; i < v.size(); i++){          //loop through blocks to find invalid block
+        if(!v[i].valid){                //if block is invalid
+            v[i].tag = tag;             //store new tag in block
+            v[i].valid = true;          //set block to valid
+            v[i].dirty = false;         //set dirty bit to false
+            block_adjust(v, i);         //adjust recency of blocks in set
+            return;                     //exit function
+        }
+    }
+    //if all blocks are valid, replace least recently used block
+    int LRU_index = 0;
+    for(int i = 0; i < v.size(); i++){
+        if(v[i].recency == v.size()){   //find index of least recently used block
+            LRU_index = i;
+        }
+    }
+    //if dirty bit is set, write back to next level of cache
+    if(v[LRU_index].dirty){
+        //write back to next level of cache
+    }
+    v[LRU_index].tag = tag;             //store new tag in block
+    v[LRU_index].valid = true;          //set block to valid
+    v[LRU_index].dirty = false;         //set dirty bit to false
+    block_adjust(v, LRU_index);         //adjust recency of blocks in set
+}
